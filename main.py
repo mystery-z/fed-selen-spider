99d9c21b209849cfe8ebd1e20f1d8257150a9d2f
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2022-03-17 23:28:45 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 27 22:35:47 2021
-
-@author: ACE-OF-DIAMONDS
-# """
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-
-# driver = webdriver.Firefox(executable_path="/home/juwoncha/문서/fed_selen/geckodriver")
-# driver.get('https://www.linuxhint.com')
-# print('Title: %s' % driver.title)
-# driver.quit()
-
-
-#https://news.google.com/search?q=%22Federal%20Reserve%22%20when%3A1d&hl=en-US&gl=US&ceid=US%3Aen 
-
 from GoogleNews import GoogleNews
 from newspaper import Article
 import pandas as pd
