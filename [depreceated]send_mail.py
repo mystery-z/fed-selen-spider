93c155f899e2c64448595e36a5bdb98625e6cdaf
mystery_in_a_hoodie <53719487+ACE-OF-DIAMONDS@users.noreply.github.com>
2022-08-04 03:52:26 +0800

    Major update ig

---
+++ [depreceated]send_mail.py
@@ -0,0 +1,43 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import timedelta
+from datetime import date
+
+today = date.today()
+
+yesterday = today - timedelta(days = 1)
+
+today_date = today.strftime("%m-%d-%Y")
+yesterday_date = yesterday.strftime("%m-%d-%Y")
+
+today_date = str(today_date)
+yesterday_date = str(yesterday_date)
+
+sender_email = "anothername@gmail.com"
+receiver_email = "name@gmail.com"
+password = "^^^^^^^"
+
+message = MIMEMultipart("alternative")
+message["Subject"] = "Federal Reserve News-" + today_date
+message["From"] = sender_email
+message["To"] = receiver_email
+
+# Create the plain-text and HTML version of your message
+html=open(today_date+"parsed.html")
+# Turn these into plain/html MIMEText objects
+part2 = MIMEText(html.read(), "html")
+
+# Add HTML/plain-text parts to MIMEMultipart message
+# The email client will try to render the last part first
+message.attach(part2)
+
+# Create secure connection with server and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(
+        sender_email, receiver_email, message.as_string()
+    )
+    
+print("mail sent successfully")
--- fed_selen_auto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 /home/put_computer_name_here/문서/fed_selen/main.py
-sleep 30s
-python3 /home/put_computer_name_here/문서/fed_selen/send_mail.py
-
